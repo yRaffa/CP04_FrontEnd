10129112cae04257dabbd5b5b1efa45f53c3c529
fecarioba <feecarioba@gmail.com>
2024-09-02 16:55:10 -0300

    pirmiras tentativas

---
+++ src/scripts/produtos.js
@@ -0,0 +1,29 @@
+// pega os dados do arquivo .json
+
+let products = null;
+fetch("produtos.json")
+  .then((response) => response.json())
+  .then((data) => {
+    products = data;
+    console.log(products);
+  });
+
+// adiciona os produtos o HTML
+
+let listProduct = document.querySelector(""); // adicionar seletor do css
+function addDataToHTML() {
+  products.forEach((products) => {
+    // cria novo elemento
+    let newProduct = document.createElement("a");
+    newProduct.href = newProduct.classList.add("item"); //referencia o arquivo html com os detalhes
+
+    // newProduct.innerHTML = `
+    //     <img src="${product.img}">
+    //     <h2>${product.name}</h2>
+    //     <div class="price">${product.price} </div>;
+    // `;
+
+    // add o elemnto na classe contida na variavel listProduct
+    listProduct.appendChild(newProduct);
+  });
+}
