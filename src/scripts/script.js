3332ac205dfedf9b9aeea43bc69c95dff6b3743a
Rafael Oliveira <yraffa.olive@gmail.com>
2024-09-04 15:21:03 -0300

    update home

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,18 @@
-// init Isotope
-var $grid = $('.collection-list').isotope({
-    // options
+// Isotope
+var $grid = $('.collection-list').isotope ({
   });
-  // filter items on button click
+
   $('.filter-button-group').on( 'click', 'button', function() {
     var filterValue = $(this).attr('data-filter');
+    resetFilterButtons();
+    $(this).addClass('active-filter-btn');
     $grid.isotope({ filter: filterValue });
   });
+
+var filterBtns = $('.filter-button-group').find('button');
+
+function resetFilterButtons() {
+    filterBtns.each(function(){
+        $(this).removeClass('active-filter-btn');
+    });
+}
