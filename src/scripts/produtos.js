2f0808a9c2a95e3f78f3d6ef06cd3baff937b70a
fecarioba <feecarioba@gmail.com>
2024-09-05 17:46:37 -0300

    finalizacao script

--- src/scripts/produtos.js
+++ src/scripts/produtos.js
@@ -6,23 +6,43 @@ fetch("/src/scripts/produtos.json")
   .then((data) => {
     products = data;
     console.log(products);
+    addDataToHTML();
   });
 // adiciona os produtos o HTML
 
-let listProduct = document.querySelector(""); // adicionar seletor do css
+let listProduct = document.querySelectorAll(".produto"); // adicionar seletor do css
 function addDataToHTML() {
-  products.forEach((products) => {
+  products.forEach((product) => {
     // cria novo elemento
     let newProduct = document.createElement("a");
-    newProduct.href = newProduct.classList.add("item"); //referencia o arquivo html com os detalhes
-
-    // newProduct.innerHTML = `
-    //     <img src="${product.img}">
-    //     <h2>${product.name}</h2>
-    //     <div class="price">${product.price} </div>;
-    // `;
+    newProduct.href = "/src/pages/descricao.html?id=" + product.id; //referencia o arquivo html com os detalhes
+    newProduct.classList.add("item");
+    newProduct.innerHTML = `
+        <img src="${product.img}">
+        <h2>${product.name}</h2>
+        <div class="price">${product.price} </div>;
+    `;
 
     // add o elemnto na classe contida na variavel listProduct
     listProduct.appendChild(newProduct);
+  });
+}
+
+let productDivs = document.querySelectorAll(".produto"); // seleciona todas as divs com a classe 'produto'
+
+function addDataToHTML() {
+  productDivs.forEach((div, index) => {
+    let newProduct = document.createElement("a");
+    newProduct.href = "/src/pages/descricao.html?id=" + products[index].id; // referencia o arquivo html com os detalhes
+    newProduct.classList.add("btn", "mt-3", "text-uppercase");
+    newProduct.innerHTML = `
+        <img src="${products[index].img}">
+        <h2>${products[index].name}</h2>
+        <div class="price">${products[index].price}</div>
+    `;
+    newProduct.textContent = "Ver Detalhes"; // define o texto do link
+
+    // adiciona o elemento <a> na div atual
+    div.appendChild(newProduct);
   });
 }
